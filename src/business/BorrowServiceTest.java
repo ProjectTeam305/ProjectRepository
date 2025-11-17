71616a9ec68110a56ebff21693fdc9fde34d762a
moodythis <moodythis.com>
2025-11-17 15:23:39 +0300

    tsfghdf

--- src/business/BorrowServiceTest.java
+++ src/business/BorrowServiceTest.java
@@ -24,7 +24,7 @@ class BorrowServiceTest {
 
     @Test
     void returnBook() {
-        borrowService.borrowBook(" user23", "bookABC");
+        borrowService.borrowBook(" user231", "bookABC");
         boolean result = borrowService.returnBook("user23", "bookABC");
         assertTrue(result, "Book should be returned successfully");
     }
